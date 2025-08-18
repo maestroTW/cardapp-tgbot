703e4c2b46c0c22deb1edecdb6083c55e698777f
maestroTW <117581948+maestroTW@users.noreply.github.com>
2025-08-19 00:12:50 +0500

    sketch
    name username avatar

--- static/script.js
+++ static/script.js
@@ -1,8 +1,18 @@
-let tg = window.Telegram.WebApp;
+document.addEventListener("DOMContentLoaded", () => {
+    let tg = window.Telegram.WebApp;
 
-function updateText() {
+    let avatar_url = tg.initDataUnsafe.photo_url;
+    //let avatar_url = 'https://ae04.alicdn.com/kf/S5dae3e85566d4f54b5dd04f90f8ea16a4.jpg_480x480.jpg'
+    let avatar = document.createElement('img');
+    avatar.src = avatar_url;
+    document.getElementById('avatar-container').appendChild(avatar);
+
+    //let username = "username";
+    //let first_name = 'ivan';
+    //let last_name = 'ivanov';
     let username = tg.initDataUnsafe.user.username;
-    let name = tg.initDataUnsafe.user.first_name + tg.initDataUnsafe.user.last_name;
-    document.getElementById('username').textContent = username
-    document.getElementById('name').textContent = name
-}
+    let first_name = tg.initDataUnsafe.user.first_name;
+    let last_name = tg.initDataUnsafe.user.last_name;
+    document.getElementById('username').textContent = username;
+    document.getElementById('name').textContent = `${first_name} ${last_name}`;
+});
