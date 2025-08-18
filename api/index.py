31dbeba2617db24d36752bd411b06121b9032212
maestroTW <117581948+maestroTW@users.noreply.github.com>
2025-08-18 23:04:19 +0500

    server test commit

--- api/index.py
+++ api/index.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
 
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return 'About'
+if __name__ == '__main__':
+    app.run(debug=True)
