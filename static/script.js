16a52e3e5d43f684722231e00fdb9ae3005ef1cc
maestroTW <117581948+maestroTW@users.noreply.github.com>
2025-08-20 21:52:40 +0500

    sketch2 (without avatar)

--- static/script.js
+++ static/script.js
@@ -2,17 +2,20 @@ document.addEventListener("DOMContentLoaded", () => {
     let tg = window.Telegram.WebApp;
 
     let avatar_url = tg.initDataUnsafe.photo_url;
-    //let avatar_url = 'https://ae04.alicdn.com/kf/S5dae3e85566d4f54b5dd04f90f8ea16a4.jpg_480x480.jpg'
     let avatar = document.createElement('img');
     avatar.src = avatar_url;
     document.getElementById('avatar-container').appendChild(avatar);
 
-    //let username = "username";
-    //let first_name = 'ivan';
-    //let last_name = 'ivanov';
     let username = tg.initDataUnsafe.user.username;
     let first_name = tg.initDataUnsafe.user.first_name;
     let last_name = tg.initDataUnsafe.user.last_name;
-    document.getElementById('username').textContent = username;
+    document.getElementById('username').textContent = `@${username}`;
     document.getElementById('name').textContent = `${first_name} ${last_name}`;
+
 });
+
+function changeBackground(gradient) {
+    document.body.style.background = gradient;
+    document.body.style.backgroundAttachment = 'fixed';
+}
+
